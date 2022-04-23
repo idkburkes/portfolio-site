0f92fbf938a1ed15199aa9ced938905d0890236e
Isaiah <idkburkes@gmail.com>
2022-04-23 16:14:32 -0500

    First commit

---
+++ components/Navbar.js
@@ -0,0 +1,230 @@
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { useTheme } from "next-themes";
+import { useRouter } from "next/router";
+import userData from "@constants/data";
+
+export default function Navbar() {
+  const router = useRouter();
+  console.log(router.asPath);
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <div className="max-w-6xl  mx-auto px-4 py-10 md:py-20">
+      <div className="flex  md:flex-row justify-between items-center">
+        {/* Logo / Home / Text */}
+
+        <div className="flex flex-col">
+          <Link href="/">
+            <a>
+              <h1 className="font-semibold text-xl dark:text-gray-100">
+                {userData.name}
+              </h1>
+              <p className="text-base font-light text-gray-500 dark:text-gray-300">
+                {userData.designation}
+              </p>
+            </a>
+          </Link>
+        </div>
+
+        <div className="space-x-8 hidden md:block">
+          <Link href="/about">
+            <a
+              className={`text-base  ${
+                router.asPath === "/about"
+                  ? "text-gray-800 font-bold dark:text-gray-400"
+                  : "text-gray-600 dark:text-gray-300 font-normal "
+              }`}
+            >
+              About{" "}
+              {router.asPath === "/about" && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-down inline-block h-3 w-3"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                  />
+                </svg>
+              )}
+            </a>
+          </Link>
+          <Link href="/projects">
+            <a
+              className={`text-base  ${
+                router.asPath === "/projects"
+                  ? "text-gray-800 font-bold dark:text-gray-400"
+                  : "text-gray-600 dark:text-gray-300 font-normal "
+              }`}
+            >
+              Projects
+              {router.asPath === "/projects" && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-down inline-block h-3 w-3"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                  />
+                </svg>
+              )}
+            </a>
+          </Link>
+          <Link href="/experience">
+            <a
+              className={`text-base  ${
+                router.asPath === "/experience"
+                  ? "text-gray-800 font-bold dark:text-gray-400"
+                  : "text-gray-600 dark:text-gray-300 font-normal "
+              }`}
+            >
+              Experience{" "}
+              {router.asPath === "/experience" && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-down inline-block h-3 w-3"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                  />
+                </svg>
+              )}
+            </a>
+          </Link>
+          <Link href="/contact">
+            <a
+              className={`text-base  ${
+                router.asPath === "/contact"
+                  ? "text-gray-800 font-bold dark:text-gray-400"
+                  : "text-gray-600 dark:text-gray-300 font-normal "
+              }`}
+            >
+              Contact
+              {router.asPath === "/contact" && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-down inline-block h-3 w-3"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                  />
+                </svg>
+              )}
+            </a>
+          </Link>
+        </div>
+
+        <div className="space-x-4 flex flex-row items-center">
+          <a
+            href={userData.socialLinks.linkedin}
+            className="text-base font-normal text-gray-600 dark:text-gray-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-linkedin h-5 w-5"
+              viewBox="0 0 16 16"
+            >
+              <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+            </svg>
+          </a>
+          <a 
+            href={userData.socialLinks.github}
+            className="text-base font-normal text-gray-600 dark:text-gray-300"
+          >
+            <svg 
+              width="27"
+              height="25"
+              viewBox="0 0 256 250"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M128.00106,0 C57.3172926,0 0,57.3066942 0,128.00106 C0,184.555281 36.6761997,232.535542 87.534937,249.460899 C93.9320223,250.645779 96.280588,246.684165 96.280588,243.303333 C96.280588,240.251045 96.1618878,230.167899 96.106777,219.472176 C60.4967585,227.215235 52.9826207,204.369712 52.9826207,204.369712 C47.1599584,189.574598 38.770408,185.640538 38.770408,185.640538 C27.1568785,177.696113 39.6458206,177.859325 39.6458206,177.859325 C52.4993419,178.762293 59.267365,191.04987 59.267365,191.04987 C70.6837675,210.618423 89.2115753,204.961093 96.5158685,201.690482 C97.6647155,193.417512 100.981959,187.77078 104.642583,184.574357 C76.211799,181.33766 46.324819,170.362144 46.324819,121.315702 C46.324819,107.340889 51.3250588,95.9223682 59.5132437,86.9583937 C58.1842268,83.7344152 53.8029229,70.715562 60.7532354,53.0843636 C60.7532354,53.0843636 71.5019501,49.6441813 95.9626412,66.2049595 C106.172967,63.368876 117.123047,61.9465949 128.00106,61.8978432 C138.879073,61.9465949 149.837632,63.368876 160.067033,66.2049595 C184.49805,49.6441813 195.231926,53.0843636 195.231926,53.0843636 C202.199197,70.715562 197.815773,83.7344152 196.486756,86.9583937 C204.694018,95.9223682 209.660343,107.340889 209.660343,121.315702 C209.660343,170.478725 179.716133,181.303747 151.213281,184.472614 C155.80443,188.444828 159.895342,196.234518 159.895342,208.176593 C159.895342,225.303317 159.746968,239.087361 159.746968,243.303333 C159.746968,246.709601 162.05102,250.70089 168.53925,249.443941 C219.370432,232.499507 256,184.536204 256,128.00106 C256,57.3066942 198.691187,0 128.00106,0 Z M47.9405593,182.340212 C47.6586465,182.976105 46.6581745,183.166873 45.7467277,182.730227 C44.8183235,182.312656 44.2968914,181.445722 44.5978808,180.80771 C44.8734344,180.152739 45.876026,179.97045 46.8023103,180.409216 C47.7328342,180.826786 48.2627451,181.702199 47.9405593,182.340212 Z M54.2367892,187.958254 C53.6263318,188.524199 52.4329723,188.261363 51.6232682,187.366874 C50.7860088,186.474504 50.6291553,185.281144 51.2480912,184.70672 C51.8776254,184.140775 53.0349512,184.405731 53.8743302,185.298101 C54.7115892,186.201069 54.8748019,187.38595 54.2367892,187.958254 Z M58.5562413,195.146347 C57.7719732,195.691096 56.4895886,195.180261 55.6968417,194.042013 C54.9125733,192.903764 54.9125733,191.538713 55.713799,190.991845 C56.5086651,190.444977 57.7719732,190.936735 58.5753181,192.066505 C59.3574669,193.22383 59.3574669,194.58888 58.5562413,195.146347 Z M65.8613592,203.471174 C65.1597571,204.244846 63.6654083,204.03712 62.5716717,202.981538 C61.4524999,201.94927 61.1409122,200.484596 61.8446341,199.710926 C62.5547146,198.935137 64.0575422,199.15346 65.1597571,200.200564 C66.2704506,201.230712 66.6095936,202.705984 65.8613592,203.471174 Z M75.3025151,206.281542 C74.9930474,207.284134 73.553809,207.739857 72.1039724,207.313809 C70.6562556,206.875043 69.7087748,205.700761 70.0012857,204.687571 C70.302275,203.678621 71.7478721,203.20382 73.2083069,203.659543 C74.6539041,204.09619 75.6035048,205.261994 75.3025151,206.281542 Z M86.046947,207.473627 C86.0829806,208.529209 84.8535871,209.404622 83.3316829,209.4237 C81.8013,209.457614 80.563428,208.603398 80.5464708,207.564772 C80.5464708,206.498591 81.7483088,205.631657 83.2786917,205.606221 C84.8005962,205.576546 86.046947,206.424403 86.046947,207.473627 Z M96.6021471,207.069023 C96.7844366,208.099171 95.7267341,209.156872 94.215428,209.438785 C92.7295577,209.710099 91.3539086,209.074206 91.1652603,208.052538 C90.9808515,206.996955 92.0576306,205.939253 93.5413813,205.66582 C95.054807,205.402984 96.4092596,206.021919 96.6021471,207.069023 Z" fill="#4b5563" />
+            </svg>
+            </a>
+          <button
+            aria-label="Toggle Dark Mode"
+            type="button"
+            className="w-10 h-10 p-3 rounded focus:outline-none"
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          >
+            {mounted && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                stroke="currentColor"
+                className="w-4 h-4 text-yellow-500 dark:text-yellow-500"
+              >
+                {theme === "dark" ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                  />
+                )}
+              </svg>
+            )}
+          </button>
+        </div>
+      </div>
+      <div className="space-x-8 block md:hidden mt-4">
+        <Link href="/about">
+          <a className="text-base font-normal text-gray-600 dark:text-gray-300">
+            About
+          </a>
+        </Link>
+        <Link href="/projects">
+          <a className="text-base font-normal text-gray-600 dark:text-gray-300">
+            Projects
+          </a>
+        </Link>
+        <Link href="/experience">
+          <a className="text-base font-normal text-gray-600 dark:text-gray-300">
+            Experience
+          </a>
+        </Link>
+        <Link href="/contact">
+          <a className="text-base font-normal text-gray-600 dark:text-gray-300">
+            Contact
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ constants/data.js
@@ -0,0 +1,109 @@
+const userData = {
+  githubUsername: "idkburkes",
+  name: "Isaiah Burkes",
+  designation: "Software Engineer",
+  avatarUrl: "/avatar.png",
+  email: "idkburkes@gmail.com",
+  phone: "+910 583 8609",
+  address: "Auburn, Alabama",
+  projects: [
+    {
+      title: "Rate-my-professor Chrome Extension",
+      link: "https://chrome.google.com/webstore/detail/auburn-university-profess/pgnfgjbmgnmfibcdlejjkkahlopajecg",
+      imgUrl: "/rmp.png",
+    },
+    {
+      title: "Full-Stack Software Development Bootcamp",
+      link: "https://github.com/idkburkes",
+      imgUrl: "/codefellows.png",
+    },
+    {
+      title: "Data Structure & Algorithm Implementations",
+      link: "https://github.com/idkburkes/blind75",
+      imgUrl: "/algos.png",
+    },
+    {
+      title: "2022 ChewyHacks Hackathon - Lessons learned from 3rd place (in progress)",
+      link: "https://github.com/idkburkes",
+      imgUrl: "/chewy.png",
+    },
+    {
+      title: "Automating deployment, scaling, and management of containerized applications (in progress)",
+      link: "https://github.com/idkburkes",
+      imgUrl: "/kubernetes.jpg",
+    },
+    {
+      title: "Authentication with OAuth2 (in progress)",
+      link: "https://github.com/idkburkes",
+      imgUrl: "/oauth2.png",
+    },
+    {
+      title: "Database Design Projects (in progress)",
+      link: "https://github.com/idkburkes",
+      imgUrl: "/database.png",
+    },   
+    {
+      title: "in progress",
+      link: "https://github.com/idkburkes",
+      imgUrl: "/algos.png",
+    },
+  ],
+  about: {
+    title:
+      "About me title",
+    description: [
+      `I am a software engineer with great enthusiasm for learning how to create scalable, fault tolerant, highly available, and maintainable software. I learn new skills quickly and have experience working on many teams.`,
+      `Interests in distributed systems, cloud computing, solving hard problems. and delivering high quality solutions that drive business impact.`,
+    ],
+    currentProject: "Full-Stack Software Development Bootcamp",
+    currentProjectUrl: "https://github.com/idkburkes",
+  },
+  experience: [
+    {
+      title: "Software Engineer",
+      company: "Microsoft",
+      year: "Current",
+      companyLink: "",
+      desc: "This is my upcoming job. I am going contribute to building the next generation of financial systems for Azure on Azure.",
+    },
+    {
+      title: "Software Engineering Intern",
+      company: "NASA Kennedy Space Center",
+      year: "Jan 22 - May 22",
+      companyLink: "https://www.nasa.gov",
+      desc: "I worked as a Software Engineering intern in NASA's Software branch of the Exploration Systems & Operations Division. I supported software development efforts to improve the Launch Control System responsible for monitoring measurement displays and sending commands to spacecraft such as the Space Launch System (SLS) and the Orion Spacecraft. I also represented NASA as a speaker on panel of 5 engineers during a High School outreach event to promote careers in STEM.",
+    },
+    {
+      title: "Software Engineering Intern",
+      company: "Integrated Solutions for Systems (IS4S)",
+      year: "Dec 20 - Aug 21",
+      companyLink: "https://is4s.com/",
+      desc: "IS4S is a Defense start-up that specializes in PNT, missile system engineering, energetic research and development, vehicle electronics, and additive manufacturing. During my 9 month internship I contributed by extending the capabilities of a Java SDK, as well as designing and developing an internal web application utilizing JavaScript, Node.js, Express.js, HTML, CSS, Docker, and MySQL. This was my first experience working alongside experienced engineers and it was amazing to see how much talent this small company has to offer.",
+    },
+    {
+      title: "Information Technology Specialist 25B",
+      company: "Florida Army National Guard",
+      year: "Jan 19 - Jan 20",
+      companyLink: "https://fl.ng.mil/Pages/Home.aspx",
+      desc: "After leaving active duty, I started my transition into the tech industry by serving in the Florida Army National Guard for one year as an IT Specialist. I started learning the ins and outs of network connectivity, software, hardware, remote desktop connections, printers, peripherals, and workstation performance. It was during this year that I earned the Cisco Certified Entry Networking Technician (CCENT) and CompTIA A+ certifications.",
+    },
+    {
+      title: "Infantryman 11B",
+      company: "United States Army",
+      year: "Sep 15 - Jan 19",
+      companyLink: "https://www.goarmy.com/",
+      desc: "I began my professional career only a few months after my 17th birthday by enlisting in the U.S. Army. I honorably served as a Paratrooper in the 82nd Airborne Division for 3 1/2 formative years. My service included ~30 static line jumps from fixed-wing and rotary-wing aircrafts, 1 combat deployment to Iraq, and countless memories with the best friends I have ever made. I believe the grit I developed as an Airborne Infantry soldier will help me succeed as a Software Engineer for your team.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com",
+    twitter: "https://twitter.com",
+    linkedin: "https://www.linkedin.com/in/isaiah-burkes/",
+    github: "https://github.com/idkburkes",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
